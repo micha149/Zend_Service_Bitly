974f7adcd7c9f6fb87bcda4f2735b720cd618711
Michael van Engelshoven <michael@van-engelshoven.de>
2010-09-09 11:15:22 +0200

    Added Exception class

---
+++ library/Zend/Service/Bitly/Exception.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Zend_Exception
+ */
+require_once 'Zend/Exception.php';
+
+
+/**
+ * @category   Zend
+ * @package    Zend_Service
+ * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class Zend_Service_Bitly_Exception extends Zend_Service_Exception
+{
+    
+}
+
